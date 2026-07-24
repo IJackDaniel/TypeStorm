911515b031c6e65ae051f1777d0c43d98de170cd
Danya <danya05af@gmail.com>
2026-07-24 17:34:36 +0300

    Добавлен JavaDoc для WordRepository

--- src/main/java/ru/dada/typestorm/repository/WordRepository.java
+++ src/main/java/ru/dada/typestorm/repository/WordRepository.java
@@ -7,12 +7,26 @@ import ru.dada.typestorm.model.Word;
 
 import java.util.List;
 
+/**
+ * Репозиторий работы с БД (таблица words)
+ */
 @Repository
 public interface WordRepository extends JpaRepository<Word, Long> {
 
+    /**
+     * Получение топа слов по частоте из определённого словаря
+     * @param dictionaryType тип словаря
+     * @param maxRank частота
+     * @return топ maxRank слов из словаря dictionaryTyoe
+     */
     List<Word> findByDictionaryTypeAndFrequencyRankLessThanEqual(
             DictionaryType dictionaryType,
             Integer maxRank);
 
+    /**
+     * Получение всех слов из словаря
+     * @param dictionaryType тип словаря
+     * @return все слова одного словаря
+     */
     List<Word> findByDictionaryType(DictionaryType dictionaryType);
 }
