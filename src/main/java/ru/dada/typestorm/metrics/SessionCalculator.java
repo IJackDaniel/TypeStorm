9630333ecc8c265ee3c2d668c4112bcdbd92de57
Danya <danya05af@gmail.com>
2026-08-17 20:16:46 +0300

    Добавлена JavaDoc для класса SessionCalculator

--- src/main/java/ru/dada/typestorm/metrics/SessionCalculator.java
+++ src/main/java/ru/dada/typestorm/metrics/SessionCalculator.java
@@ -5,6 +5,9 @@ import ru.dada.typestorm.model.TypingSession;
 
 import java.time.Duration;
 
+/**
+ * Класс обработки сессии
+ */
 public class SessionCalculator {
     private final WpmCalculator wpmCalculator;
     private final AccuracyCalculator accuracyCalculator;
@@ -18,6 +21,11 @@ public class SessionCalculator {
         this.accuracyCalculator = accuracyCalculator;
     }
 
+    /**
+     * Функция обработки сессии
+     * @param session одна тренировка печати
+     * @return класс, содержащий подсчитанные результаты сессии
+     */
     public TypingResult calculate(TypingSession session) {
         if (!session.isFinished()) {
             throw new IllegalArgumentException("Session is not finished");
@@ -33,15 +41,31 @@ public class SessionCalculator {
         return new TypingResult(session, wpm, accuracy, duration);
     }
 
+    /**
+     * Подсчёт длительности сессии
+     * @param session сессия
+     * @return длительность сессии
+     */
     private int calculateDuration(TypingSession session) {
         Duration duration = Duration.between(session.getStartTime(), session.getEndTime());
         return (int) duration.getSeconds();
     }
 
+    /**
+     * Подсчёт точности печати в сессии
+     * @param session сессия
+     * @return точность
+     */
     private double calculateAccuracy(TypingSession session) {
         return this.accuracyCalculator.calculateAccuracy(session.getOriginalText(), session.getEnteredText());
     }
 
+    /**
+     * Подсчёт скорости печати в сессии
+     * @param session сессия
+     * @param duration длительность сессии
+     * @return скорость печати (WPM - слов в минуту)
+     */
     private double calculateWpm(TypingSession session, int duration) {
         int symbolsCount = session.getEnteredText().length();
         return this.wpmCalculator.calculateWpm(duration, symbolsCount);
